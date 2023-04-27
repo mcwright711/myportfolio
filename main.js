4390679e2faf5c9c795702182d0e16d592a83ad8
mcwright711 <118693689+mcwright711@users.noreply.github.com>
2023-04-27 17:49:45 -0600

    updated navbar, responsive
    hamburger menu does not open and close, jquery problems

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
-$(document).on("scroll", function(){
-    var document=$(document).scrollTop();
-    if(topOfWindow>10){
-        $(".ulnav").addClass("ulnav-white");
-    }else{
-        $(".ulnav").removeClass("lnav-white");
-    }
+$(document).ready(function(){
+$(window).scroll(function(){
+    if($(document).scrollTop() > 50){
+    $("ulnav").addclass("-scrolled");
+}else{
+    $("ulnav").removeClass("-scrolled");
+  }
+ });
 });
+/*Toggle between adding and removing responsive class to ulnav when the user clicks on the icon*/
+function myFunction(){
+    var x = document.getElementbyclass("icon");
+    if (x.className === "ulnav a"){
+        x.className +="responsive a";
+    }else{
+        x.Classname="ulnav a";
+    }
+}
