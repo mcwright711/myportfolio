6d8f89f7d6ec3de9fff00c7e8d123533e2c69596
mcwright711 <118693689+mcwright711@users.noreply.github.com>
2023-04-27 20:28:35 -0600

    redone jquery

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
 $(document).ready(function(){
 $(window).scroll(function(){
     if($(document).scrollTop() > 50){
-    $("ulnav").addclass("-scrolled");
+    $("ulnav").addclass("scrolled");
 }else{
-    $("ulnav").removeClass("-scrolled");
+    $("ulnav").removeClass("scrolled");
   }
  });
 });
 /*Toggle between adding and removing responsive class to ulnav when the user clicks on the icon*/
-function myFunction(){
-    var x = document.getElementbyclass("icon");
-    if (x.className === "ulnav a"){
-        x.className +="responsive a";
-    }else{
-        x.Classname="ulnav a";
-    }
-}
+$(".ulnav a").click (function() {
+    // $(this).find(".ulnav a.responsive").addclass(".active");
+    $(this).find(".ulnav a.responsive.icon").addclass(".active");
+    $(this).siblings(".ulnav a").find("ulnav a.responsive").removeClass(".active");
+}); 
