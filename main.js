55e8f18e8b7d81c0975d8fbb07a1eb73728a283f
mcwright711 <118693689+mcwright711@users.noreply.github.com>
2023-04-27 21:39:55 -0600

    updates 4/27
    trying to get jquery and mediaqueries to work sitewide

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
-$(document).ready(function(){
 $(window).scroll(function(){
-    if($(document).scrollTop() > 50){
-    $("ulnav").addclass("scrolled");
+    if($(window).scrollTop() > 50){
+    $(".ulnav").addclass(".scrolled");
 }else{
-    $("ulnav").removeClass("scrolled");
+    $(".ulnav").removeClass(".scrolled");
   }
  });
-});
 /*Toggle between adding and removing responsive class to ulnav when the user clicks on the icon*/
 $(".ulnav a").click (function() {
     // $(this).find(".ulnav a.responsive").addclass(".active");
-    $(this).find(".ulnav a.responsive.icon").addclass(".active");
-    $(this).siblings(".ulnav a").find("ulnav a.responsive").removeClass(".active");
+    $(this).find(".a.icon").addclass("active");
+    $(this).siblings(".a").find("a.responsive").removeClass("active");
 }); 
