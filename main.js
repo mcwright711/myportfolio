ca1e16cc076c0de4157c12c348541cb8df8bb6d2
mcwright711 <118693689+mcwright711@users.noreply.github.com>
2023-04-29 19:53:20 -0600

    icon is back, disappears when clicked
    when the icon is a link, it shows the navigation briefly and then the page breaks.  However, when I had the hamburger menu as an icon, it wouldn't show up at all.  fiddled with css and jquery.  I'm going to attempt to make it a button next.

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
-$(window).scroll(function(){
-    if($(window).scrollTop() > 50){
-    $(".ulnav").addclass(".scrolled");
+$(document).ready(function(){
+    $(window).scroll(function() {
+        if ($(this).scrollTop()>0){
+    $('.ulnav').toggleClass('scrolled');
 }else{
-    $(".ulnav").removeClass(".scrolled");
+    $('.ulnav').removeClass('scrolled');
   }
  });
+});
+
 /*Toggle between adding and removing responsive class to ulnav when the user clicks on the icon*/
-$(".ulnav a").click (function() {
-    // $(this).find(".ulnav a.responsive").addclass(".active");
-    $(this).find(".a.icon").addclass("active");
-    $(this).siblings(".a").find("a.responsive").removeClass("active");
-}); 
+$(document).ready(function(){
+$('.icon').click(function(){
+    $('.ulnav a').toggleClass('responsive');
+});
+});
